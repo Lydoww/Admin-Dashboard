40afa226b6f7bc6d2b7ee9d438893df4fb990bf4
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-03-19 06:44:24 +0100

    common header

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,12 +1,13 @@
-import { div } from 'framer-motion/client'
-import React from 'react'
+import { div } from "framer-motion/client";
+import React from "react";
+import Header from "../components/common/Header";
 
 const OverviewPage = () => {
   return (
-    <div className='flex-1 overflow-y-auto relative z-10'>
-      <Header />
+    <div className="flex-1 overflow-y-auto relative z-10">
+      <Header title="Overview" />
     </div>
-  )
-}
+  );
+};
 
-export default OverviewPage
+export default OverviewPage;
