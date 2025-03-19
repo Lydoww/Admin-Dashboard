1af88bb099f8fcb3db9ad369488b1bc6b24db1e3
Alexis HELM <128365251+Lydovw@users.noreply.github.com>
2025-03-19 10:45:00 +0100

    sales page

--- src/components/common/StatCard.jsx
+++ src/components/common/StatCard.jsx
@@ -9,7 +9,7 @@ const StatCard = ({ name, icon: Icon, value, color }) => {
       <div className="px-4 py-5 sm:p-6">
         <span className="flex items-center text-sm font-medium text-gray-400">
           <Icon size={20} className="mr-2" style={{ color }} />
-          {name}
+            {name}
         </span>
         <p className="mt-1 text-3xl font-semibold text-gray-100">{value}</p>
       </div>
